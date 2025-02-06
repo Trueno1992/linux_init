261d97a8721d780125730d546441ffc79697ed84
qinyang.dy <qinyang.dy@autonavi.com>
2025-02-06 10:43:58 +0800

    小修改

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo yum -y install curl
+sudo yum -y install gcc
+sudo yum -y install gcc-c++
+sudo yum -y install gdb
+sudo yum -y install lrzsz
+sudo yum -y install git
+sudo yum -y install docker
